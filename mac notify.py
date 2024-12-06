8b4e6db35cbedfd4c16319b1f48c022a0e41d1d0
Dhruv Rao <57631631+rao-dhruv@users.noreply.github.com>
2024-12-06 14:09:59 -0500

    Add files via upload

---
+++ mac notify.py
@@ -0,0 +1,5 @@
+import os
+while True:
+    os.system("""
+        osascript -e 'display notification "Python is the best Language!" with title "Python Notification"'
+    """)
---
+++ win notify.py
@@ -0,0 +1,8 @@
+from plyer import notification  
+notification.notify(
+    title = "Python",
+    message = "Python is the best Language!",
+    app_icon = None,
+    app_name = "Python Notification",
+    timeout = 10
+)
